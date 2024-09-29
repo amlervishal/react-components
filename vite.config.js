5e6b4b61c689c5acf760720efecb856c74340952
gisgurgaon2020 <106843134+amlervishal@users.noreply.github.com>
2024-09-28 21:22:39 -0400

    setting up gh pages ans docs build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-components/',
+  build: {
+    outDir: 'docs'
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
